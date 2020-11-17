78af8d7eefbd0502878146301491e5230fa34151
alamdar121 <alamdar.abass52@gmail.com>
2020-11-17 23:44:38 +0530

    Create removeParticularCharcter.java

---
+++ removeParticularCharcter.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {  
+              String str = "India is my country";  
+              System.out.println(charRemoveAt(str, 7));  
+           }  
+           public static String charRemoveAt(String str, int p) {  
+              return str.substring(0, p) + str.substring(p + 1);  
+           }  
+}
